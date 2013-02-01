ce6c455ea77e07d4ee2fc66733eaef96e273ded0
Kyle Treubig <kyle@vimofthevine.com>
2013-01-31 19:09:04 -0500

    Corrects file header copyright year from 2012 to 2013

--- client/src/ScanForm.cpp
+++ client/src/ScanForm.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Kyle Treubig
+ * Copyright 2013 Kyle Treubig
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
